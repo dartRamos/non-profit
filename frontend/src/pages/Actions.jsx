31bea15df28c077d9fbeb413d33305efaf9bdebb
Dartanhan Ramos <alessandroramosd@gmail.com>
2026-05-04 23:11:52 -0400

    fixed image fade issue

--- frontend/src/pages/Actions.jsx
+++ frontend/src/pages/Actions.jsx
@@ -71,7 +71,7 @@ export default function Actions() {
           <img src={rectangle} className="events-bg" alt="background" />
 
           <div className="events-overlay">
-            <div className="image-fade-2" />
+          
             {tab === "cta" && <ActionFeed type="cta" />}
             {tab === "petitions" && <ActionFeed type="petition" />}
             {tab === "email" && <ActionFeed type="email" />}
