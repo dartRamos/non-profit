07ca6f88ac2f9cf118869eaa51326fd8e39f4e9f
Dartanhan Ramos <alessandroramosd@gmail.com>
2026-05-05 21:22:56 -0400

    adding analytics tracker

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
